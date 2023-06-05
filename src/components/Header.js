638fc107db4b018a95c3c62cd2e89464201cf121
RafayShams <rafayshams@gmail.com>
2023-06-04 20:25:51 -0400

    Add animation to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 function Header() {
   return (
     <header>
+      <div className="leftLight"></div>
+      <div className="rightLight"></div>
         <h1>Movie Reviewer</h1>
         <h2>All your movie reviews in one place!</h2>
     </header>
